e9397bae8c87e29230baa5d52c5bbef769c1e1e2
sundawei2018 <dsun2016@gmail.com>
2018-12-18 01:29:28 -0500

    check user info

--- src/component/Authroute/authroute.js
+++ src/component/Authroute/authroute.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
+@withRouter
 class AuthRoute extends Component {
-
+    
     componentDidMount() {
+        const publicList = ['/login','/register'];
+        const pathname = this.props.location.pathname;
+        
+        if (publicList.indexOf(pathname) > -1) {
+            return null;
+        }
         // user information
         axios.get('/user/info')
             .then(res => {
                 if (res.status == 200) {
-                    console.log(res.data);
+                    if (res.data.code == 0) {
+
+                    }
+                    else {
+                        this.props.history.push('./login');
+                    }
                 }
             })
         // 
