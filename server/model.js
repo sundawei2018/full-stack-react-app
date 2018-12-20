4bbe3e7d0492ddab1b680efc6714c78f4b34b990
sundawei2018 <dsun2016@gmail.com>
2018-12-20 01:20:20 -0500

    finish register

--- server/model.js
+++ server/model.js
@@ -1,4 +1,35 @@
-const User = mongoose.model('user', new mongoose.Schema({
-    user: {type: String, required:true},
-    age: {type: Number, required: true}
-}));
+const mongoose = require('mongoose');
+const DB_URL = 'mongodb://localhost:27017/react-chat';
+mongoose.connect(DB_URL);
+
+const models = {
+    user: {
+        'user': {type: String, require: true},
+        'pwd' : {type: String, require: true},
+        'type': {type: String, require: true},
+        'avatar': {type: String},
+        'desc': {type: String},
+        'title': {type: String},
+
+        // employer
+        'company': {type: String},
+        'salary': {type: String}
+    },
+    chat: {
+    }
+}
+
+for (let m in models) {
+    mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+	getModel:function(name){
+		return mongoose.model(name);
+	}
+}
+
+// const User = mongoose.model('user', new mongoose.Schema({
+//     user: {type: String, required:true},
+//     age: {type: Number, required: true}
+// }));
--- server/user.js
+++ server/user.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const Router = express.Router();
 
+const model = require('./model');
+const User = model.getModel('user');
+
+Router.get('/list', function(req, res) {
+    User.find({}, function(err, doc) {
+        return res.json(doc);
+    });
+}); 
+
+Router.post('/register', function(req, res) {
+    console.log(req.body);
+    const {user, pwd, type} = req.body;
+    User.findOne({user: user}, function(err, doc) {
+        if (doc) {
+            return res.json({code:1, msg:'user already exists'});
+        }
+        User.create({user, pwd, type}, function(err, doc) {
+            if (err) {
+                return res.json({code:1, msg: 'something wrong'});
+            }
+            return res.json({code: 0});
+        });
+    });
+});
+
 Router.get('/info', function(req, res) {
     // check cookie
     return res.json({code : 1});
